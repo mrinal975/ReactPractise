625231020e1d210e582064884ea9fca894716cf2
mrinal975 <mrinalmallik1@gmail.com>
2022-09-25 06:45:12 +0600

    bootstrap added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,12 +3,13 @@ import React, { Component } from "react";
 class Counter extends Component{
 
     state = {
-        count:0
+        count:0,
+        imageUrl:'https://img.freepik.com/premium-photo/cut-view-hot-sexy-couple-window-bed-room-strong-guy-hold-woman-red-lingerie-himself-she-sit-embrace-guy-with-legs_152404-11257.jpg'
     };
 
     render(){
         return (<React.Fragment>
-            <span>{this.formatCounter()}</span>
+            <span className="badge badge-primary m-2">{this.formatCounter()}</span>
             <button>Increment</button>
             </React.Fragment>
         );
