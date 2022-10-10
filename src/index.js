50f23b466d45bd00c5b176a1003ba9b7a9acab71
mrinal975 <mrinalmallik1@gmail.com>
2022-10-10 17:32:55 +0600

    function is called as component

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import 'bootstrap/dist/css/bootstrap.css';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
-const element = (
-  <h1 className='heading'>
-    <span> Hello World {new Date().toLocaleDateString()}</span>
-  </h1>
-);
+function Clock(){
+  return (
+    <h1 className='heading'>
+      <span> Hello World {new Date().toLocaleTimeString()}</span>
+    </h1>
+  );
+}
 
-root.render(element);
+root.render(Clock())
+
 reportWebVitals();
 
 // ReactDOM.render(element, document.getElementById('root'))
