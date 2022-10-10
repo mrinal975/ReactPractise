d69de77f4a6b95c5d04ed357c933d5d5a007f835
mrinal975 <mrinalmallik1@gmail.com>
2022-10-10 17:22:27 +0600

    upgrage

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 const element = (
   <h1 className='heading'>
-    <span> Hello World</span>
+    <span> Hello World {new Date().toLocaleDateString()}</span>
   </h1>
 );
 
-ReactDOM.render(element, document.getElementById('root'))
+root.render(element);
+reportWebVitals();
+
+// ReactDOM.render(element, document.getElementById('root'))
