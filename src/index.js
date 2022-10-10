9dd4ffd8804ce42d8a91c6768400454211602728
mrinal975 <mrinalmallik1@gmail.com>
2022-10-10 21:18:41 +0600

    class component

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
-function Clock({local}){
-  return (
-    <h1 className='heading'>
-      <span> Hello World {new Date().toLocaleTimeString(local)}</span>
-    </h1>
-  );
+class Clock extends React.Component {
+  render(){ 
+    return (
+      <h1 className='heading'>
+        <span> Hello World {new Date().toLocaleTimeString()}</span>
+      </h1>
+    );
+  }  
 }
 
-root.render(
-  <Clock local="bn-BD"/>
-)
+const ClockComponent =  new Clock();
+root.render(<Clock/>);
 
 reportWebVitals();
 
