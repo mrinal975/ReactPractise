d683e026819322b8b906871764091efb8c227257
mrinal975 <mrinalmallik1@gmail.com>
2022-10-10 16:04:31 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Counter from './components/counter';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Counter/>
+import ReactDOM from 'react-dom';
+const element = (
+  <h1 className='heading'>
+    <span> Hello World</span>
+  </h1>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(element, document.getElementById('root'))
