ba85526d5b0a7efed98daed818ee94f08afbef93
mrinal975 <mrinalmallik1@gmail.com>
2022-10-11 14:56:08 +0600

    functional to class component

--- src/components/TemperateInput.js
+++ src/components/TemperateInput.js
@@ -1,34 +1,17 @@
-import React from "react";
-
 const scaleNames = {
     c:'Celsius',
     f:'Fahrenheit'
 };
 
-export default class TemperateInput extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            temperature: ''
-        };
-    }
-    
-    handleChange=(e)=>{
-        this.setState({
-            temperature : e.target.value,
-        });
-    }
-
-    render(){
-        const {temperature} = this.state;
-        const {scale} = this.props;
-        return(
-            <fieldset>
-                <legend>Enter Temperate in {scaleNames[scale]}:</legend>
-                <input value={temperature} 
-                onChange={this.handleChange}>
-                </input>
-            </fieldset>
-        );
-    }
+export default function TemperateInput({temperature, scale, onTemperatureChange})
+{
+    return(
+        <fieldset>
+            <legend>Enter Temperate in {scaleNames[scale]}:</legend>
+            <input type="text" value={temperature} 
+            onChange={onTemperatureChange}>
+            </input>
+        </fieldset>
+    );
+   
 }
