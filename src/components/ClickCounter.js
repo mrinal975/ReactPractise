f02ec869b2038ec6b6eed4c2d3c988edfcb0c3fb
mrinal975 <mrinalmallik1@gmail.com>
2022-10-11 17:34:54 +0600

    higher order function

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,26 +1,12 @@
-import React from "react";
-
-class ClickCounter extends React.Component{
-    state = {
-        count :0,
-    };
-    incrementCount=()=>{
-        this.setState((prevState)=>({
-            count: prevState.count +1
-        }));
-    }
-
-    render(){
-        const { count}  = this.state;
-
-        return (
-            <div>
-                <button type="button" onClick={this.incrementCount}>
-                    Clicked {count} times
-                </button>
-            </div>
-        );
-    }
+import WithCounter from "./HOC/WithCounter";
+const ClickCounter = (props)=>{
+    const { count, incrementCount}  = props;
+    return (
+        <div>
+            <button type="button" onClick={incrementCount}>
+                Clicked {count} times
+            </button>
+        </div>
+    );
 }
-
-export default ClickCounter;
+export default WithCounter(ClickCounter);
--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -1,26 +1,12 @@
-import React from "react";
-
-class HoverCounter extends React.Component{
-    state = {
-        count :0,
-    };
-    incrementCount=()=>{
-        this.setState((prevState)=>({
-            count: prevState.count +1
-        }));
-    }
-
-    render(){
-        const { count}  = this.state;
-
-        return (
-            <div>
-                <h1 type="button" onMouseOver={this.incrementCount}>
-                    Hovered {count} times
-                </h1>
-            </div>
-        );
-    }
+import WithCounter from "./HOC/WithCounter";
+const HoverCounter = (props)=>{
+    const { count, incrementCount}  = props;
+    return (
+        <div>
+            <h1 onMouseOver={incrementCount}>
+                Clicked {count} times
+            </h1>
+        </div>
+    );
 }
-
-export default HoverCounter;
+export default WithCounter(HoverCounter);
