8736dc0939135b89ba6cc92a1f4e4df69499a35d
mrinal975 <mrinalmallik1@gmail.com>
2022-10-11 16:51:51 +0600

    click counter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 // import ClockList from "./components/ClockList";
 // import Form from "./components/Form";
+// import Calculator from "./components/Calculator";
+import ClickCounter from "./components/ClickCounter";
 
-import Calculator from "./components/Calculator";
-
-
-
-const quantities = [1,2,3];
+// const quantities = [1,2,3];
 function App() {
   return (
     <div>
       {/* <ClockList quantities={quantities}/> */}
       {/* <Form></Form> */}
-      <Calculator></Calculator>
+      {/* <Calculator></Calculator> */}
+      <ClickCounter></ClickCounter>
       </div>
   );
 }
