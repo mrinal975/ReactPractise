884c9a16c00ff1fb7434ae847f1fccaa5e63cae0
mrinal975 <mrinalmallik1@gmail.com>
2022-10-11 15:07:45 +0600

    convrter created

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,13 +1,21 @@
 import React from "react";
 import TemperateInput from "./TemperateInput";
 export default class Calculator extends React.Component{
-    handleChange=(e)=>{
+    state = {
+            temperature:'',
+            scale:'c'
+        };
+        
+    handleChange=(e, scale)=>{
         this.setState({
             temperature : e.target.value,
+            scale,
         });
     }
 
     render(){
+        const {temperature, scale} = this.state;
+        
         return(
             <div>
                 <TemperateInput scale="c"/>
