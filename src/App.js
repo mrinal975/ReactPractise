f2970127b6ef63a409495fd728c3c2c80e46aa90
mrinal975 <mrinalmallik1@gmail.com>
2022-10-12 22:57:56 +0600

    multiple useResucer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 // import Counter from "./components/Reducer/Counter";
-
-import ComplexCounter from "./components/Reducer/ComplexCounter";
+// import ComplexCounter from "./components/Reducer/ComplexCounter";
+import CounterThree from "./components/Reducer/CounterThree";
 function App() {
 
   return (
     <div className="app">
       {/* <Counter></Counter> */}
-      <ComplexCounter></ComplexCounter>
+      {/* <ComplexCounter></ComplexCounter> */}
+      <CounterThree></CounterThree>
     </div>
   );
 }
