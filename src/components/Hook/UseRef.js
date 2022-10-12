4ba6ef34d7e5623bc187ab3167a71cbbce02eff1
mrinal975 <mrinalmallik1@gmail.com>
2022-10-12 17:34:26 +0600

    hook added

---
+++ src/components/Hook/UseRef.js
@@ -0,0 +1,12 @@
+function UseRef(){
+
+    return (
+        <div>
+            <p>
+                <input type="text" placeholder ="Enter something"/>
+            </p>
+        </div>
+    );
+}
+
+export default UseRef;
