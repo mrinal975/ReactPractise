5197ec248c874e6f9c8b7215aae44d3d3eaba601
mrinal975 <mrinalmallik1@gmail.com>
2022-10-12 22:10:13 +0600

    hook use reducer

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
-// import ClockList from "./components/ClockList";
-// import Form from "./components/Form";
-// import Calculator from "./components/Calculator";
-// import ClickCounter from "./components/ClickCounter";
-// import HoverCounter from "./components/HoverCounter";
-// const quantities = [1,2,3];
+import Counter from "./components/Reducer/Counter";
 
-// import RenderPropsWithCounter from "./components/HOC/RenderPropsWithCounter";
-// import RenderPropsClickCounter from "./components/RenderPropsClickCounter";
-// import RenderPropsHoverCounter from "./components/RenderPropsHoverCounter";
-// import RenderPropsHoverCounter from "./components/RenderPropsHoverCounter";
-// import Todo from "./components/Todo";
-// import { useState } from "react";
-// import UseEffectHook from "./components/UseEffectHook";
 function App() {
-  
-  // const [show, setShow] =useState(true);
 
   return (
     <div className="app">
-      {/* <ClockList quantities={quantities}/> */}
-      {/* <Form></Form> */}
-      {/* <Calculator></Calculator> */}
-      {/* <ClickCounter/>*/}
-      {/* <HoverCounter/> */}
-
-      {/* <RenderPropsWithCounter>
-        {(counter,incrementCount )=>(
-          <RenderPropsClickCounter count={counter} incrementCount = {incrementCount}/>
-        )}
-      </RenderPropsWithCounter>
-      <RenderPropsWithCounter>
-        {(counter, incrementCount)=>(
-          <RenderPropsHoverCounter count={counter} incrementCount = {incrementCount}/>
-        )}
-      </RenderPropsWithCounter> */}
-      {/* <Todo/> */}
-      {/*<div>
-         {show && <UseEffectHook/>}
-        <p>
-          <button type="button" onClick={(e)=>setShow((prevShow=>!prevShow))}>
-            {show ? 'Hide post' : 'Show post'}
-          </button>
-        </p>
-      </div> */}
-      
-      </div>
+      <Counter></Counter>
+    </div>
   );
 }
 
