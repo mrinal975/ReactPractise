a0cc3eb13bdb2c7bfbd795d07bd6b1e40d4d1d81
mrinal975 <mrinalmallik1@gmail.com>
2022-10-13 08:14:05 +0600

    form control

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
-
-
-// import Calculator from "./components/Calculator";
-// import ClickCounter from "./components/ClickCounter";
-// import HoverCounter from "./components/HoverCounter";
-// import RenderPropsWithCounter from "./components/HOC/RenderPropsWithCounter";
-// import RenderPropsClickCounter from "./components/RenderPropsClickCounter";
-// import RenderPropsHoverCounter from "./components/RenderPropsHoverCounter";
-// import RenderPropsHoverCounter from "./components/RenderPropsHoverCounter";
-// import Todo from "./components/Todo";
-// import { useState } from "react";
-// import UseEffectHook from "./components/UseEffectHook";
+import Form from "./components/Form";
 function App() {
-  // const [show, setShow] =useState(true);
 
   return (
     <div className="app">
-      
-      
-      {/* <Calculator></Calculator> */}
-      {/* <ClickCounter/>*/}
-      {/* <HoverCounter/> */}
-
-      {/* <RenderPropsWithCounter>
-        {(counter,incrementCount )=>(
-          <RenderPropsClickCounter count={counter} incrementCount = {incrementCount}/>
-        )}
-      </RenderPropsWithCounter>
-      <RenderPropsWithCounter>
-        {(counter, incrementCount)=>(
-          <RenderPropsHoverCounter count={counter} incrementCount = {incrementCount}/>
-        )}
-      </RenderPropsWithCounter> */}
-      {/* <Todo/> */}
-      {/*<div>
-         {show && <UseEffectHook/>}
-        <p>
-          <button type="button" onClick={(e)=>setShow((prevShow=>!prevShow))}>
-            {show ? 'Hide post' : 'Show post'}
-          </button>
-        </p>
-      </div> */}
-      
-      </div>
+      <Form></Form>
+    </div>
   );
 }
 
--- src/components/BoilingVerdict.js
+++
@@ -1,6 +0,0 @@
-export default function BoilingVerdict({celsius=0}){
-    if(celsius>=100){
-        return <p>The Water would be boil</p>
-    }
-    return <p>The Water would not boil</p>
-}
