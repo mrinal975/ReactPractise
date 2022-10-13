afb22f704046a574394a109d00207b33e155eaf7
mrinal975 <mrinalmallik1@gmail.com>
2022-10-13 08:22:12 +0600

    render props

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@
 // import Calculator from "./components/Calculator";
 // import ClickCounter from "./components/ClickCounter";
 // import HoverCounter from "./components/HoverCounter";
-// import RenderPropsWithCounter from "./components/RenderPropsWithCounter";
-// import RenderPropsClickCounter from "./components/RenderPropsClickCounter";
-// import RenderPropsHoverCounter from "./components/RenderPropsHoverCounter";
-// import RenderPropsHoverCounter from "./components/RenderPropsHoverCounter";
 // import Todo from "./components/Todo";
 // import { useState } from "react";
 // import UseEffectHook from "./components/UseEffectHook";
@@ -20,17 +16,6 @@ function App() {
       {/* <Calculator></Calculator> */}
       {/* <ClickCounter/>*/}
       {/* <HoverCounter/> */}
-
-      {/* <RenderPropsWithCounter>
-        {(counter,incrementCount )=>(
-          <RenderPropsClickCounter count={counter} incrementCount = {incrementCount}/>
-        )}
-      </RenderPropsWithCounter>
-      <RenderPropsWithCounter>
-        {(counter, incrementCount)=>(
-          <RenderPropsHoverCounter count={counter} incrementCount = {incrementCount}/>
-        )}
-      </RenderPropsWithCounter> */}
       {/* <Todo/> */}
       {/*<div>
          {show && <UseEffectHook/>}
