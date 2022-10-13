c0364e43543d2d8467c04dafc04de889be22ae73
mrinal975 <mrinalmallik1@gmail.com>
2022-10-13 13:03:46 +0600

    update

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import styles from './assets/css/Logo.module.css';
 
 export default function Logo(){
+    const [glassColor, setGlassColor] = useState('black');
+
+    const pickColorRandomly = ()=>{
+        const colors = ['red','green', 'yellow', 'blue', 'orange'];
+        const color = colors[Math.floor(Math.random()*colors.length)];
+        return color;
+    };
 
     return (
         <div className="logo">
             <div className="hair">
-                
+                <p>Hair on there</p>
             </div>
             <div className={styles.glasses}>
-                <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 103.63 42.68"/>
+                <p style={{color:glassColor}}>Glasses on there</p>
             </div>
             <div className="play">
-                <button type="button">Colorize Glass</button>
-                <button type="button">Reset Glass</button>
+                <button type="button" onClick={()=>setGlassColor(pickColorRandomly())}>Colorize Glass</button>
+                <button type="button" onClick={()=>setGlassColor('blue')}>Reset Glass</button>
             </div>
         </div>
     );
