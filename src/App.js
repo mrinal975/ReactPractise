bc7d03bfe82f2119b3d54fdceba5eee8bc86ca25
mrinal975 <mrinalmallik1@gmail.com>
2022-10-13 08:43:19 +0600

    higher order component(HOC)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 // import Calculator from "./components/Calculator";
-// import ClickCounter from "./components/ClickCounter";
 // import HoverCounter from "./components/HoverCounter";
 // import Todo from "./components/Todo";
 function App() {
   return (
     <div className="app">
       {/* <Calculator></Calculator> */}
-      {/* <ClickCounter/>*/}
       {/* <HoverCounter/> */}
       {/* <Todo/> */}
     </div>
