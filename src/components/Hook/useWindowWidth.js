7a971e7e60ad52626753b1f500cde2aa40cf5f8b
mrinal975 <mrinalmallik1@gmail.com>
2022-10-13 09:27:39 +0600

    cutom hook window width

--- src/components/Hook/useWindowWidth.js
+++ src/components/Hook/useWindowWidth.js
@@ -1,21 +1,20 @@
 import { useEffect, useState } from "react";
 
-export default function LayoutComponentTwo(){
-    
+const useWindowWidth =()=>{
+
     const [onSmallScreen, setOnSmallScreen] = useState(false);
     const checkScreenSize = ()=>{
         setOnSmallScreen(window.innerWidth < 768);
     }
-
+    
     useEffect(()=>{
         checkScreenSize();
         window.addEventListener('resize',checkScreenSize);
         return () => window.removeEventListener('resize',checkScreenSize);
     },[])
 
-    return (
-        <div className={onSmallScreen? 'small' : 'large' }>
-            <h1>Your are browsing on  device</h1>
-        </div>
-    );
+    return onSmallScreen;
 }
+
+export default useWindowWidth;
+
--- src/components/LayoutComponent.js
+++ src/components/LayoutComponent.js
@@ -1,20 +1,8 @@
-import { useEffect, useState } from "react";
+import useWindowWidth from "./Hook/useWindowWidth";
 
 export default function LayoutComponent(){
     
-    const [onSmallScreen, setOnSmallScreen] = useState(false);
-    const checkScreenSize = ()=>{
-        setOnSmallScreen(window.innerWidth < 768);
-    }
-
-    useEffect(()=>{
-        console.log('hi  ---');
-        checkScreenSize();
-        window.addEventListener('resize',checkScreenSize);
-        console.log('hi 2  ---');
-        return () => window.removeEventListener('resize',checkScreenSize);
-    },[])
-
+    const onSmallScreen = useWindowWidth()
     return (
         <div>
             <h1>Your are browsing on {onSmallScreen? 'small' : 'large' } device</h1>
