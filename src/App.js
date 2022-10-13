ddcc75b22ef1fda376f2509b9c0ea0aa10e3a77a
mrinal975 <mrinalmallik1@gmail.com>
2022-10-13 08:51:23 +0600

    higher order component removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 // import Calculator from "./components/Calculator";
-// import HoverCounter from "./components/HoverCounter";
 // import Todo from "./components/Todo";
 function App() {
   return (
     <div className="app">
       {/* <Calculator></Calculator> */}
-      {/* <HoverCounter/> */}
       {/* <Todo/> */}
     </div>
   );
