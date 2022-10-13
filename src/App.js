50cf07535534ea0792751a8ae4b854e51191b232
mrinal975 <mrinalmallik1@gmail.com>
2022-10-13 12:10:59 +0600

    remove unnecessary

--- src/App.js
+++ src/App.js
@@ -1,37 +1,9 @@
-// import ClockList from "./components/ClockList";
 import UseRef from "./components/Hook/UseRef";
 function App() {
-  
   // const [show, setShow] =useState(true);
 
   return (
     <div className="app">
-      {/* <ClockList quantities={quantities}/> */}
-      {/* <Form></Form> */}
-      {/* <Calculator></Calculator> */}
-      {/* <ClickCounter/>*/}
-      {/* <HoverCounter/> */}
-
-      {/* <RenderPropsWithCounter>
-        {(counter,incrementCount )=>(
-          <RenderPropsClickCounter count={counter} incrementCount = {incrementCount}/>
-        )}
-      </RenderPropsWithCounter>
-      <RenderPropsWithCounter>
-        {(counter, incrementCount)=>(
-          <RenderPropsHoverCounter count={counter} incrementCount = {incrementCount}/>
-        )}
-      </RenderPropsWithCounter> */}
-      {/* <Todo/> */}
-      {/*<div>
-         {show && <UseEffectHook/>}
-        <p>
-          <button type="button" onClick={(e)=>setShow((prevShow=>!prevShow))}>
-            {show ? 'Hide post' : 'Show post'}
-          </button>
-        </p>
-      </div> */}
-      
       <UseRef></UseRef>
       </div>
   );
