343dc7409fedd1b6dc387072c9ffd4fe67382849
mrinal975 <mrinalmallik1@gmail.com>
2022-10-13 08:25:03 +0600

    import element

---
+++ src/components/RenderPropsClickCounter.js
@@ -0,0 +1,23 @@
+import React from "react";
+export default class RenderPropsClickCounter extends React.Component{
+    state = {
+        count :0,
+    };
+
+    incrementCount= () => {
+        this.setState((prevState)=>({
+            count: prevState.count +1
+        }));
+    }
+
+    render(){
+        const {count} = this.state;
+        return (
+            <div>
+                <button type="button" onClick={this.incrementCount}>
+                    Clicked {count} times
+                </button>
+            </div>
+        );
+    }
+}
