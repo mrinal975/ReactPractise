aee3733a7cbc45ccc58abd024d320da35c1132cc
mrinal975 <mrinalmallik1@gmail.com>
2022-10-16 14:01:35 +0600

    update code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,8 +8,8 @@ export default function Nav(){
             <ul>
                 <li>
                 <a href="index.html" className={classes.brand}>
-                    <img src={logo} alt="Learn with Sumit Logo" />
-                    <h3>Learn with Sumit</h3>
+                    <img src={logo} alt="react " />
+                    <h3>Learning React</h3>
                 </a>
                 </li>
             </ul>
