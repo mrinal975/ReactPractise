418217cd6cee5db691130c5e7f2e4bb8ba2a3a14
mrinal975 <mrinalmallik1@gmail.com>
2022-10-17 08:17:57 +0600

    upadte

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Layout from './components/Layout';
-import Login from './components/Pages/Login';
-import Signup from './components/Pages/Signup';
 import './styles/App.css';
 function App() {
   return (
     <div className="app">
       <Layout>
+        <h1>dfskjgsajkb</h1>
       </Layout>
-      <Login></Login>
     </div>
     
   );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,15 @@
 import classes from '../styles/Layout.module.css';
 import Nav from "./Nav";
-export default function Layout({Children}){
 
+export default function Layout({Children}){
+    console.log(Children, 'Children');
     return (
         <>
-        <Nav></Nav>
+        <Nav/>
         <main className={classes.main}>
             <div className={classes.container}>
                 {Children}
             </div>
-
         </main>
         </>
         
