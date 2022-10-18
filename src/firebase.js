4201ba633ecc9cc803ad62a4235be833e1c441f1
mrinal975 <mrinalmallik1@gmail.com>
2022-10-18 12:12:19 +0600

    database added

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,7 @@ const app = initializeApp({
     apiKey: "AIzaSyCaN8JD-9y5URBtQ1iJxXnqRDSTatLH26k",
     authDomain: "react-quiz-f2f7d.firebaseapp.com",
     projectId: "react-quiz-f2f7d",
+    databaseURL: "https://react-quiz-f2f7d-default-rtdb.asia-southeast1.firebasedatabase.app",
     storageBucket: "react-quiz-f2f7d.appspot.com",
     messagingSenderId: "32319217903",
     appId: "1:32319217903:web:8bce18485050888326bb1a"
