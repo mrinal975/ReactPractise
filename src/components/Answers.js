84d7128741d19a124b346b79b250b27bfb1aafa3
mrinal975 <mrinalmallik1@gmail.com>
2022-10-19 15:53:58 +0600

    update

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,10 +1,20 @@
 import Checkbox from "./Checkbox";
 
 import classes from '../styles/Answers.module.css';
-export default function Answers(){
+export default function Answers({options=[], handleChange }){
     return (
         <div className={classes.answers}>
-            <Checkbox className={classes.answer} text="Test answer" />
+            {options.map((option,index)=>
+            (
+                <Checkbox 
+                className={classes.answer} 
+                text={option.title} 
+                value ={index} 
+                checkbox={option.checked} 
+                onChange={(e)=>handleChange(e, index)}/>
+            )
+            )}
+            
         </div>
     );
 }
