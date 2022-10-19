a702ee66819a688eeea61bfffcd420eabf455266
mrinal975 <mrinalmallik1@gmail.com>
2022-10-19 16:23:19 +0600

    quiz question option  setup

--- src/components/Pages/Quiz.js
+++ src/components/Pages/Quiz.js
@@ -1,26 +1,77 @@
-import { useState } from "react";
+import _ from "lodash";
+import { useEffect, useReducer, useState } from "react";
 import { useParams } from "react-router-dom";
 import useQuestion from "../../hooks/useQuestion";
 import Answers from "../Answers";
 import MiniPlayer from "../MiniPlayer";
 import ProgressBar from "../ProgressBar";
-
 export default function Quiz(){
 
     const {id} = useParams();
-    const {loading, error, question} = useQuestion(id);
+    const {loading, error, questions} = useQuestion(id);
     const [currentQuestion, setCurrentQuestion] = useState(0);
     
+    const initialState = null;
+    const reducer = (state, action) =>{
+    
 
+        switch(action.type){
+            case "questions":
+                action.value.forEach((question)=>{
+                    question.options.forEach((option)=>{
+                        option.checked = false;
+                    })
+                });
+                return action.value;
+            case "answer":
+                const questions = _.cloneDeep(state);
+                questions[action.questionID].options[action.optionIndex].checked = action.value;
+                return questions;
+            default:
+                return state;
+        }
+    }
+    
+    const [qna, dispatch] = useReducer(reducer, initialState);
+    
+
+    useEffect(()=>{
+        dispatch({
+            type:"questions",
+            value: questions
+        })
+    },[questions]);
+
+
+    function handleAnswerChange(e, index){
+        dispatch({
+            type:"answer",
+            questionID: currentQuestion,
+            optionsIndex: index,
+            value: e.target.checked
+        })
+    }
+    
+    
     return (
         <>
-            <div className="container">
-                <h1>Pick three of your favorite Star Wars Flims</h1>
-                <h4>Question can have multiple answers</h4>
-                <Answers/>
-                <ProgressBar/>
-                <MiniPlayer/>
-            </div>
+            {error && <div> There was an error. </div>}
+            {loading && <div> Loading... </div>}
+            {!loading && !error && qna && qna.length>0 && (
+                <>
+                    <div className="container">
+                    <h1>{qna[currentQuestion].title}</h1>
+                    <h4>Question can have multiple answers</h4>
+                    <Answers 
+                    options={qna[currentQuestion].options} 
+                    handleChange={handleAnswerChange}/>
+                    
+                    <ProgressBar/>
+                    <MiniPlayer/>
+                    </div>
+                </>
+            )}
+           
         </>
     );
 }
