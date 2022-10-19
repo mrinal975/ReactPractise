516fe1f48ebe00d543bf9839ee192e3471e5b637
mrinal975 <mrinalmallik1@gmail.com>
2022-10-20 00:45:20 +0600

    progrss bar toolkit

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,15 +1,42 @@
+import { useRef, useState } from 'react';
 import classes from '../styles/ProgressBar.module.css';
 import Button from './Button';
 export default function ProgressBar({next, prev, submit, percentage}){
+  const [tooltip, setTooltip] = useState(false);
+  const tooltipRef = useRef();
+
+  function toggleTooltip(){
+    if(tooltip){
+      setTooltip(false);
+      tooltipRef.current.style.display = "none";
+    }
+    else{
+      setTooltip(true);
+      tooltipRef.current.style.left =`calc(${progress}% - 65px)`;
+      tooltipRef.current.style.display = "block";
+      
+    }
+  }
+
     return (
         <div className={classes.progressBar}>
           <div className={classes.backButton}>
             <span className="material-icons-outlined" onClick={prev}> arrow_back </span>
           </div>
           <div className={classes.rangeArea}>
-            <div className={classes.tooltip}>{percentage}% Cimplete!</div>
+            <div 
+            className={classes.tooltip}
+            ref={tooltipRef}
+            >
+              {percentage}% Complete!
+            </div>
             <div className={classes.rangeBody}>
-              <div className={classes.progress} style={{width:`${percentage}%`}}></div>
+              <div 
+              className={classes.progress} 
+              style={{width:`${percentage}%`}}
+              onMouseOver={toggleTooltip}
+              onMouseOut={toggleTooltip}
+              ></div>
             </div>
           </div>
             <Button className={classes.next} onClick={percentage===100?submit:next}>
